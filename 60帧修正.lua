9c93dc11f387a82d30173b0ebbbb9aaad15d37f4
Richard.Kiriko <52127286+RichardKiriko@users.noreply.github.com>
2019-06-29 01:25:50 +0800

    Add files via upload

---
+++ 60帧修正.lua
@@ -0,0 +1,23 @@
+﻿local tr = aegisub.gettext
+
+script_name = tr"修正"
+script_description = tr"修正60帧对帧出现的偏差"
+script_author = "Kiriko"
+script_version = "1"
+
+function xz(subs)
+    for i = 1, #subs do
+        if subs[i].class == "dialogue" then
+			local line=subs[i]
+	        if line.start_time%50 == 0 and line.start_time ~= 0 then
+		        line.start_time=line.start_time+10
+	        end
+	        if line.end_time%50 == 0 then
+		        line.end_time=line.end_time+10
+	        end
+	        subs[i]=line			
+        end
+    end
+end
+
+aegisub.register_macro(script_name, script_description, xz)
